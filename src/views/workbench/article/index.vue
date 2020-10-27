a3c9e6571259d5fe6f8d22ab312ca31550fbd6c6
syb <>
2020-10-28 00:19:21 +0800

    travis two

--- src/views/workbench/article/index.vue
+++ src/views/workbench/article/index.vue
@@ -35,7 +35,7 @@ export default {
 					items: [
 						{
 							prop: 'title',
-							label: '文章标题travis',
+							label: '文章标题travis two',
 							span: 24,
 							component: {
 								name: 'el-input',
